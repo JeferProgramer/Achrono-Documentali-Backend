e4e7e522ccbc5195c45f8a543d37122936d79a23
JeferProgramer <jefer.talento.11@gmail.com>
2023-03-11 12:37:53 -0500

    add build

---
+++ build/routes/userClient/userClientRoutes.js
@@ -0,0 +1,21 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const express_1 = require("express");
+const signIn_1 = __importDefault(require("./signIn"));
+const { getUserClient, createUserClient, deleteUserClient, putUserClient, getPsychologistDetails, googleLogin } = require('./userClient');
+const validateClient = require('../../middleware/validateClient');
+const validateAdmin = require('../../middleware/ValidateAdminToken');
+const clientRouter = (0, express_1.Router)();
+const jwt = require("jsonwebtoken");
+const upload = require('../../middleware/upload');
+clientRouter.get('/client', validateClient, getUserClient);
+clientRouter.get('/:IdUserPsychologist', validateClient, getPsychologistDetails);
+clientRouter.post('/client/register', upload.single('profileImage'), createUserClient);
+clientRouter.post('/client/login', signIn_1.default);
+clientRouter.delete('/deleteuserclient', validateClient, deleteUserClient);
+clientRouter.put('/editprofile', upload.single('profileImage'), validateClient, putUserClient);
+//Falta middleware solo de admin
+module.exports = clientRouter;
